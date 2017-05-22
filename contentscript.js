97a54aad0168b93530a830ce204d8f183d874e96
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2017-05-22 11:34:38 +0300

    advanced hiding logic

--- contentscript.js
+++ contentscript.js
@@ -1,2 +1,29 @@
-var advDom = document.querySelector("#ads_left");
-if (advDom) advDom.style.display = "none";
+function waitForElementToDisplay(selector, time, cb) {
+    var target = document.querySelector(selector);
+    if( target!=null ) {
+        return cb(target);
+    }  else {
+        setTimeout(function() {
+            waitForElementToDisplay(selector, time);
+        }, time);
+    }
+}
+
+var MutationObserver = window.WebKitMutationObserver;
+var adsSelector = "#ads_left";
+
+waitForElementToDisplay(adsSelector, 2000, function (target) {
+
+    if (!target) return;
+
+    target.style.display = "none";
+
+    var adsElementObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            target.style.display = "none";
+        });
+    });
+
+    adsElementObserver.observe(target, { attributes: true, attributeOldValue: true });
+
+});
